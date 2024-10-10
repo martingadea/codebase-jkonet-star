fe6f2a2f855b8a447f5af45e02a707cb8fb6ddf3
Antonio Terpin <antonio.terpin@gmail.com>
2024-10-10 12:17:26 +0200

    Fix loadfromwandb module

--- utils/load_from_wandb.py
+++ utils/load_from_wandb.py
@@ -6,7 +6,8 @@ def parse_name(run_name: str) -> Dict[str, str]:
     Parses the run name and extracts model configuration details.
 
     The run name is expected to follow the pattern:
-    <method>.potential_<potential>_internal_<internal>_beta_<beta>_interaction_<interaction>_dt_<dt>_T_<T>_dim_<dim>_N_<N>_gmm_<gmm>_seed_<seed>_split_<split>
+    <method>.potential_<potential>_internal_<internal>_beta_<beta>_interaction_<interaction>
+        _dt_<dt>_T_<T>_dim_<dim>_N_<N>_gmm_<gmm>_seed_<seed>_split_<split>
 
 
     Parameters
@@ -18,18 +19,19 @@ def parse_name(run_name: str) -> Dict[str, str]:
     -------
     Dict[str, str]
         A dictionary with the extracted configuration details:
-        - 'method': The method used in the run.
-        - 'potential': Type of potential used.
-        - 'internal': Type of internal process used.
-        - 'beta': Beta parameter value.
-        - 'interaction': Type of interaction used.
-        - 'dt': Time step size.
-        - 'T': Total time or steps.
-        - 'dim': Dimensionality.
-        - 'N': Number of samples.
-        - 'gmm': Gaussian Mixture Model parameter.
-        - 'seed': Random seed used.
-        - 'split': Data split parameter.
+
+        - `method`: The method used in the run.
+        - `potential`: Type of potential used.
+        - `internal`: Type of internal process used.
+        - `beta`: Beta parameter value.
+        - `interaction`: Type of interaction used.
+        - `dt`: Time step size.
+        - `T`: Total time or steps.
+        - `dim`: Dimensionality.
+        - `N`: Number of samples.
+        - `gmm`: Gaussian Mixture Model parameter.
+        - `seed`: Random seed used.
+        - `split`: Data split parameter.
     """
     # Regular expression to capture the values
     pattern = re.compile(r'(?P<method_value>.*?).potential_(?P<potential_value>.*?)_internal_(?P<internal_value>.*?)_beta_(?P<beta>[0-9.]+)_interaction_(?P<interaction_value>.*?)_dt_(?P<dt>[0-9.]+)_T_(?P<T>[0-9.]+)_dim_(?P<dim>[0-9.]+)_N_(?P<N>[0-9.]+)_gmm_(?P<gmm>[0-9.]+)_seed_(?P<seed>[0-9.]+)(?:_split_(?P<split>[0-9.]+))?')
