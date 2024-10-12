7c262e90298aaac3b6039a662560b45ab9e84da5
Antonio Terpin <antonio.terpin@gmail.com>
2024-10-12 18:37:00 +0200

    Add scripts for rna experiments

---
+++ scripts/exp_rna_jkonet_vanilla.sh
@@ -0,0 +1,7 @@
+python preprocess_rna_seq.py --n-components 5
+python data_generator.py --load-from-file RNA_PCA_5 --test-ratio 0.4
+
+for seed in 0 1 2 3 4
+do
+    python train.py --dataset RNA_PCA_5 --solver jkonet-vanilla --seed $seed --wandb
+done
