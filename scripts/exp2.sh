53a4798d44c17d0aace1255fff63d33c59cea052
Antonio Terpin <antonio.terpin@gmail.com>
2024-10-15 00:40:50 +0200

    update parallel

--- scripts/exp2.sh
+++ scripts/exp2.sh
@@ -4,7 +4,7 @@ n_particles_values=(2000 5000 10000 15000 20000)
 
 export potentials dims n_particles_values
 
-parallel -j 15 "
+parallel -j 8 "
     potential={1}; dim={2}; n_particles={3};
     python data_generator.py --potential \$potential --n-particles \$n_particles --test-ratio 0.5 --split-trajectories --dimension \$dim &&
     python train.py --solver jkonet-star-potential --dataset potential_\${potential}_internal_none_beta_0.0_interaction_none_dt_0.01_T_5_dim_\${dim}_N_\${n_particles}_gmm_10_seed_0_split_0.5_split_trajectories_True --wandb
--- scripts/exp3.sh
+++ scripts/exp3.sh
@@ -5,7 +5,7 @@ betas=(0.0 0.1 0.2)
 
 export potentials interactions betas
 
-parallel -j 15 "
+parallel -j 8 "
     potential={1}; interaction={2}; beta={3};
     python data_generator.py --potential \$potential --interaction \$interaction --n-particles 2000 --test-ratio 0.5 --internal wiener --beta \$beta --split-trajectories &&
     python train.py --solver jkonet-star --dataset potential_\${potential}_internal_wiener_beta_\${beta}_interaction_\${interaction}_dt_0.01_T_5_dim_2_N_2000_gmm_10_seed_0_split_0.5_split_trajectories_True --wandb &&
